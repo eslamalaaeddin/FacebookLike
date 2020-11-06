53c82c93dc05b4e24ee16b6375f463aa818a3159
Islam Alaa Eddin <alaaeslam98@yahoo.com>
2020-11-07 00:36:13 +0200

    adding navigation component

---
+++ app/src/main/java/com/example/facebook_clone/ui/activity/SplashActivity.kt
@@ -0,0 +1,14 @@
+package com.example.facebook_clone.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.facebook_clone.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+
+    }
+}
