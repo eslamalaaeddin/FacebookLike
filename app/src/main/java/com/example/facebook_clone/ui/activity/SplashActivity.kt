0c66fefcf63b70402cd2836d4f13e8de5939fc43
Islam Alaa Eddin <alaaeslam98@yahoo.com>
2020-11-07 01:47:05 +0200

    adding splash activity

--- app/src/main/java/com/example/facebook_clone/ui/activity/SplashActivity.kt
+++ app/src/main/java/com/example/facebook_clone/ui/activity/SplashActivity.kt
@@ -1,7 +1,11 @@
 package com.example.facebook_clone.ui.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.Window
+import android.view.WindowManager
 import com.example.facebook_clone.R
 
 class SplashActivity : AppCompatActivity() {
@@ -9,6 +13,26 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
+        makeFullScreen()
 
+        Handler().postDelayed({
+//            if (currentUser == null){
+//                startActivity(Intent(this, RegisterActivity::class.java))
+//            }else{
+//                startActivity(Intent(this, WelcomeActivity::class.java))
+//            }
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            finish()
+
+        }, 3000)
+    }
+
+    private fun makeFullScreen() {
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+        supportActionBar?.hide()
     }
 }
