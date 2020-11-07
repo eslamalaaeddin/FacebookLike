c097d5798675c90189a400dcc814c325d76f6a4e
Islam Alaa Eddin <alaaeslam98@yahoo.com>
2020-11-07 14:19:58 +0200

    implementing register activity

--- app/src/main/java/com/example/facebook_clone/ui/activity/RegisteringActivity.kt
+++ app/src/main/java/com/example/facebook_clone/ui/activity/RegisteringActivity.kt
@@ -2,11 +2,28 @@ package com.example.facebook_clone.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.NavHost
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.facebook_clone.R
+import kotlinx.android.synthetic.main.toolbar_layout.*
 
 class RegisteringActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registering)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragmentContainer) as NavHostFragment
+        navController = navHostFragment.findNavController()
+        setSupportActionBar(toolbar)
+        setupActionBarWithNavController(navController)
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
